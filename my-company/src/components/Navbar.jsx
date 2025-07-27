1b8810999c887c9c3b8f8a57aac709f1424dcfde
tarikua <tarikuasileshi@gmail.com>
2025-07-27 19:30:59 +0300

    Update Navbar.jsx

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,6 +2,16 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
+     <nav
+      style={{
+        backgroundColor: '#333',
+        padding: '10px',
+        marginBottom: '20px',
+        display: 'flex',
+        justifyContent: 'space-around',
+        alignItems: 'center'
+      }}
+    >
     <nav style={{ backgroundColor: '#333', padding: '10px', marginBottom: '20px' }}>
       <Link to="/" style={{ color: 'white', marginRight: '15px' }}>Home</Link>
       <Link to="/about" style={{ color: 'white', marginRight: '15px' }}>About</Link>
