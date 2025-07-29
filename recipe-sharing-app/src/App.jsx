379ad553ddc33db8e59606aef179fbc745704867
tarikua <tarikuasileshi@gmail.com>
2025-07-29 15:08:44 +0300

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,15 +1,30 @@
-import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
 
 function App() {
   return (
     <div className="App">
       <h1>🍽️ Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <RecipeList />
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <AddRecipeForm />
+              <RecipeList />
+            </>
+          }
+        />
+        <Route path="/recipes/:id" element={<RecipeDetails />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
