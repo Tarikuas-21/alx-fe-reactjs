13a76297415f8e6bc408233ab0c8aff3d5708767
tarikua <tarikuasileshi@gmail.com>
2025-07-31 15:43:34 +0300

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
-const headers = {
-  Authorization: `token ${import.meta.env.VITE_APP_GITHUB_API_KEY}`,
+
+const BASE_URL = import.meta.env.VITE_APP_GITHUB_API_URL || 'https://api.github.com';
+
+export const fetchUserData = async (username) => {
+  const response = await axios.get(`${BASE_URL}/users/${username}`);
+  return response.data;
 };
-const response = await axios.get(`${BASE_URL}/users/${username}`, { headers });
+
