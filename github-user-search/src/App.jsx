f582438fb36feb94f097208d1a2b063481e4cedc
tarikua <tarikuasileshi@gmail.com>
2025-07-31 15:18:33 +0300

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,1 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', paddingTop: '2rem' }}>
+      <h1>GitHub User Search</h1>
+      <p>building GitHub User Search interface.</p>
+    </div>
+  );
+}
+
+export default App;
 
