320c1b4d2d3e6c8f78f8a11ca8934e265ab8e3e3
tarikua <tarikuasileshi@gmail.com>
2025-08-04 12:53:30 +0300

    Update RecipeList.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { useRecipeStore } from '../store/recipeStore';
 
 const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.filteredRecipes);
   const filteredRecipes = useRecipeStore(state => state.filteredRecipes);
 
   return (
